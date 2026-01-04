bfc6fa9cddbc952dcbd9d2aec855fd83cb0dde00
Inasaya Flanderin <InasayaFlanderin@gmail.com>
2026-01-04 15:23:25 +0700

    Initial Load

--- src/main/java/com/myteam/work/gui/pages/ManagerPage.java
+++ src/main/java/com/myteam/work/gui/pages/ManagerPage.java
@@ -450,7 +450,9 @@ public class ManagerPage extends JPanel {
 			if (searchBar.getText().equals(defaultText))
 				loadAll.run();
 			else
-				search.accept(searchBar.getText());
+			 	search.accept(searchBar.getText());
+			
+			submitWin.dispose();
 		});
 		submitWin.setCancelAction(_ -> submitWin.dispose());
 	}
